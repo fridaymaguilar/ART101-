46c7cee8c86332dc134054acb22f1be54d59868b
fridaymaguilar <fridaymaguilar@gmail.com>
2025-04-21 17:58:28 -0700

    pusgg

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Claw Machine 
+//implimentation of a game 
+// Author: Frida Aguilar <Fmaguila@ucsc.edu>
+// Date: April 21 2025 
 
 // Constants
 
 // Functions
+
+// Display claw machine container
+// Accept user input
+// Validate user input
+	  // Insert coin
+    // Move claw left/right using the joystick according to player input
+    // Drop claw according to player input
+    // Run randomized win-lose condition
+// Lift claw
+// Drop prize (if grabbed)
+// Reset for next player
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
